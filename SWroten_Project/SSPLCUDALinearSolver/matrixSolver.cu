2afbb5b7cb3f658a85f6c34a88cfe69188731215
Scott Wroten <swroten@udel.edu>
2017-10-14 14:38:48 -0400

    continuous integration

--- SWroten_Project/SSPLCUDALinearSolver/matrixSolver.cu
+++ SWroten_Project/SSPLCUDALinearSolver/matrixSolver.cu
@@ -21,133 +21,8 @@ __host__ cudaEvent_t get_time(void)
 	return time;
 }
 
-__global__ void max(int *maxValueIndexArray,
-	const double *inputMatrixColumnArray,
-	const int columnIndexInMatrix,
-	const int numberOfElements)
-{
-	// Create Shared Memory Array
-	extern volatile __shared__ double sharedDataArray[];
-	extern volatile __shared__ int sharedDataArrayIndices[];
-
-	// Initialize Shared Data Array Values to 0
-	sharedDataArray[threadIdx.x] = inputMatrixColumnArray[threadIdx.x];
-
-	// Sychronize all Threads
-	__syncthreads();
-
-	// Check if Block Size is greater than 512
-	if (BLOCKSIZE >= 512)
-	{
-		if (threadIdx.x < 256)
-		{
-			// Update shared memory 
-			sharedDataArray[threadIdx.x] = (sharedDataArray[threadIdx.x] > sharedDataArray[threadIdx.x + 256]) ?
-				sharedDataArray[threadIdx.x] : sharedDataArray[threadIdx.x + 256];
-			sharedDataArrayIndices[threadIdx.x] = (sharedDataArray[threadIdx.x] > sharedDataArray[threadIdx.x + 256]) ?
-				threadIdx.x : threadIdx.x + 256;
-		}
-
-		// Sychronize all Threads
-		__syncthreads();
-	}
-
-	if (BLOCKSIZE >= 256)
-	{
-		if (threadIdx.x < 128)
-		{
-			// Update shared memory 
-			sharedDataArray[threadIdx.x] = (sharedDataArray[threadIdx.x] > sharedDataArray[threadIdx.x + 128]) ?
-				sharedDataArray[threadIdx.x] : sharedDataArray[threadIdx.x + 128];
-			sharedDataArrayIndices[threadIdx.x] = (sharedDataArray[threadIdx.x] > sharedDataArray[threadIdx.x + 128]) ?
-				threadIdx.x : threadIdx.x + 128;
-		}
-
-		// Sychronize all Threads
-		__syncthreads();
-	}
-
-	if (BLOCKSIZE >= 128)
-	{
-		if (threadIdx.x < 64)
-		{
-			// Update shared memory 
-			sharedDataArray[threadIdx.x] = (sharedDataArray[threadIdx.x] > sharedDataArray[threadIdx.x + 64]) ?
-				sharedDataArray[threadIdx.x] : sharedDataArray[threadIdx.x + 64];
-			sharedDataArrayIndices[threadIdx.x] = (sharedDataArray[threadIdx.x] > sharedDataArray[threadIdx.x + 64]) ?
-				threadIdx.x : threadIdx.x + 64;
-		}
-
-		// Sychronize all Threads
-		__syncthreads();
-	}
-
-	if (threadIdx.x < 32)
-	{
-		if (BLOCKSIZE >= 64)
-		{
-			// Update shared memory 
-			sharedDataArray[threadIdx.x] = (sharedDataArray[threadIdx.x] > sharedDataArray[threadIdx.x + 32]) ?
-				sharedDataArray[threadIdx.x] : sharedDataArray[threadIdx.x + 32];
-			sharedDataArrayIndices[threadIdx.x] = (sharedDataArray[threadIdx.x] > sharedDataArray[threadIdx.x + 32]) ?
-				threadIdx.x : threadIdx.x + 32;
-		}
-
-		if (BLOCKSIZE >= 32)
-		{
-			// Update shared memory 
-			sharedDataArray[threadIdx.x] = (sharedDataArray[threadIdx.x] > sharedDataArray[threadIdx.x + 16]) ?
-				sharedDataArray[threadIdx.x] : sharedDataArray[threadIdx.x + 16];
-			sharedDataArrayIndices[threadIdx.x] = (sharedDataArray[threadIdx.x] > sharedDataArray[threadIdx.x + 16]) ?
-				threadIdx.x : threadIdx.x + 16;
-		}
-
-		if (BLOCKSIZE >= 16)
-		{
-			// Update shared memory 
-			sharedDataArray[threadIdx.x] = (sharedDataArray[threadIdx.x] > sharedDataArray[threadIdx.x + 8]) ?
-				sharedDataArray[threadIdx.x] : sharedDataArray[threadIdx.x + 8];
-			sharedDataArrayIndices[threadIdx.x] = (sharedDataArray[threadIdx.x] > sharedDataArray[threadIdx.x + 8]) ?
-				threadIdx.x : threadIdx.x + 8;
-		}
-
-		if (BLOCKSIZE >= 8)
-		{
-			// Update shared memory 
-			sharedDataArray[threadIdx.x] = (sharedDataArray[threadIdx.x] > sharedDataArray[threadIdx.x + 4]) ?
-				sharedDataArray[threadIdx.x] : sharedDataArray[threadIdx.x + 4];
-			sharedDataArrayIndices[threadIdx.x] = (sharedDataArray[threadIdx.x] > sharedDataArray[threadIdx.x + 4]) ?
-				threadIdx.x : threadIdx.x + 4;
-		}
-
-		if (BLOCKSIZE >= 4)
-		{
-			// Update shared memory 
-			sharedDataArray[threadIdx.x] = (sharedDataArray[threadIdx.x] > sharedDataArray[threadIdx.x + 2]) ?
-				sharedDataArray[threadIdx.x] : sharedDataArray[threadIdx.x + 2];
-			sharedDataArrayIndices[threadIdx.x] = (sharedDataArray[threadIdx.x] > sharedDataArray[threadIdx.x + 2]) ?
-				threadIdx.x : threadIdx.x + 2;
-		}
-
-		if (BLOCKSIZE >= 2)
-		{
-			// Update shared memory 
-			sharedDataArray[threadIdx.x] = (sharedDataArray[threadIdx.x] > sharedDataArray[threadIdx.x + 1]) ?
-				sharedDataArray[threadIdx.x] : sharedDataArray[threadIdx.x + 1];
-			sharedDataArrayIndices[threadIdx.x] = (sharedDataArray[threadIdx.x] > sharedDataArray[threadIdx.x + 1]) ?
-				threadIdx.x : threadIdx.x + 1;
-		}
-	}
-
-	if (threadIdx.x == 0)
-	{
-		maxValueIndexArray[columnIndexInMatrix] = sharedDataArrayIndices[0];
-	}
-}
-
 __global__ void ludecomposition(int *pivotMatrix,
 	double *matrix,
-	int *maxValueIndexMatrix,
 	const int numberOfElements,
 	const int squareMatrixDimension)
 {
@@ -155,41 +30,43 @@ __global__ void ludecomposition(int *pivotMatrix,
 	const int columnIndexInMatrix = (blockIdx.x * blockDim.x) + threadIdx.x;
 
 	// Create Shared Memory Array
-	extern volatile __shared__ double columnValues[];
+	extern volatile __shared__ int maxColumnIndices[];
 
 	// Exit if thread index is greater than number of elements
 	if (columnIndexInMatrix >= numberOfElements) { return; }
 
 	// Initialize Column Values in context of this column
+	double largestValue = 0;
 	double matrixCurrentColumnValue = 0;
 	double matrixLargestColumnValue = 0;
 	int pivotMatrixCurrentColumnValue = 0;
 	int pivotMatrixLargestColumnValue = 0;
 
-#pragma unroll
 	// Get all row elements in current column 
 	for (int rowIndex = 0; rowIndex < squareMatrixDimension; rowIndex++)
-	{
-		columnValues[rowIndex] = matrix[(rowIndex * squareMatrixDimension) + columnIndexInMatrix];
+	{			
+		// Get Max Row Value in Column
+		if (matrix[(rowIndex * squareMatrixDimension) + columnIndexInMatrix] > largestValue)
+		{
+			largestValue = matrix[(rowIndex * squareMatrixDimension) + columnIndexInMatrix];
+			maxColumnIndices[columnIndexInMatrix] = rowIndex;
+		}
 	}
 
 	// Sync Threads
 	__syncthreads();
-
-	// Get Max of Column Values
-	//max << <1, squareMatrixDimension >> >(maxValueIndexMatrix, columnValues, columnIndexInMatrix, numberOfElements);
-
+	
 	// Update Pivot Matrix Indices and Values
 	pivotMatrixCurrentColumnValue = pivotMatrix[columnIndexInMatrix];
-	pivotMatrixLargestColumnValue = pivotMatrix[maxValueIndexMatrix[columnIndexInMatrix]];
+	pivotMatrixLargestColumnValue = pivotMatrix[maxColumnIndices[columnIndexInMatrix]];
 	pivotMatrix[columnIndexInMatrix] = pivotMatrixLargestColumnValue;
-	pivotMatrix[maxValueIndexMatrix[columnIndexInMatrix]] = pivotMatrixCurrentColumnValue;
+	pivotMatrix[maxColumnIndices[columnIndexInMatrix]] = pivotMatrixCurrentColumnValue;
 
 	// Each Column Will Exchange Current Column Row Element with Pivot Element
 	matrixCurrentColumnValue = matrix[(columnIndexInMatrix * squareMatrixDimension) + columnIndexInMatrix];
-	matrixLargestColumnValue = matrix[(maxValueIndexMatrix[columnIndexInMatrix] * squareMatrixDimension) + columnIndexInMatrix];
+	matrixLargestColumnValue = matrix[(maxColumnIndices[columnIndexInMatrix] * squareMatrixDimension) + columnIndexInMatrix];
 	matrix[(columnIndexInMatrix * squareMatrixDimension) + columnIndexInMatrix] = matrixLargestColumnValue;
-	matrix[(maxValueIndexMatrix[columnIndexInMatrix] * squareMatrixDimension) + columnIndexInMatrix] = matrixCurrentColumnValue;
+	matrix[(maxColumnIndices[columnIndexInMatrix] * squareMatrixDimension) + columnIndexInMatrix] = matrixCurrentColumnValue;
 
 #pragma unroll
 	// Schur Complement
@@ -208,10 +85,6 @@ __global__ void ludecomposition(int *pivotMatrix,
 	__syncthreads();
 }
 
-
-
-
-
 float InvertMatrix(double *cpuInvertedMatrix,
 	const double *cpuMatrix,
 	const int numberOfElements,
@@ -221,10 +94,9 @@ float InvertMatrix(double *cpuInvertedMatrix,
 	cudaError_t error;
 	double *gpuMatrix = 0;
 	int *gpuPivotMatrix = 0;
-	int *gpuMaxValueIndexMatrix = 0;
 	float timeToCompleteInMs = 0;
+	double *gpuInvertedMatrix = 0;
 	int *cpuPivotMatrix = (int *)malloc(squareMatrixDimension * sizeof(int));
-	int *cpuMaxValueIndexMatrix = (int *)malloc(squareMatrixDimension * sizeof(int));
 
 	// Verify that Machine has GPU Installed by 
 	//  selecting first GPU available.
@@ -247,8 +119,8 @@ float InvertMatrix(double *cpuInvertedMatrix,
 		return 0;
 	}
 
-	// Allocate GPU Memory for max value matrix
-	if (cudaMalloc((void**)&gpuMaxValueIndexMatrix, numberOfElements * sizeof(int)) != cudaSuccess)
+	// Allocate GPU Memory for input matrix
+	if (cudaMalloc((void**)&gpuInvertedMatrix, numberOfElements * sizeof(double)) != cudaSuccess)
 	{
 		// Print error regarding inability to allocate GPU Device Memory
 		fprintf(stderr, "Error: Failed to allocate memory on CUDA capable GPU device!");
@@ -256,7 +128,7 @@ float InvertMatrix(double *cpuInvertedMatrix,
 		// exit before executing any further
 		return 0;
 	}
-
+	
 	// Allocate GPU Memory for input Pivot Matrix
 	if (cudaMalloc((void**)&gpuPivotMatrix, squareMatrixDimension * sizeof(int)) != cudaSuccess)
 	{
@@ -271,17 +143,16 @@ float InvertMatrix(double *cpuInvertedMatrix,
 	for (int i = 0; i < squareMatrixDimension; i++)
 	{
 		cpuPivotMatrix[i] = i;
-		cpuMaxValueIndexMatrix[i] = 0;
 	}
 
 	// Copy Matrix Data From CPU Memory to GPU Memory
 	cudaMemcpy(gpuMatrix, cpuMatrix, numberOfElements * sizeof(double), cudaMemcpyHostToDevice);
 	cudaMemcpy(gpuPivotMatrix, cpuPivotMatrix, squareMatrixDimension * sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(gpuMaxValueIndexMatrix, cpuMaxValueIndexMatrix, numberOfElements * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(gpuInvertedMatrix, cpuInvertedMatrix, numberOfElements * sizeof(double), cudaMemcpyHostToDevice);
 
 	// Launch Kernel in context of each column in matrix for computing matrix LU Decomposition
 	//  returns LU Matrix and Pivot Matrix for 
-	ludecomposition<<<1, squareMatrixDimension, squareMatrixDimension * sizeof(int) >> >(gpuPivotMatrix, gpuMatrix, gpuMaxValueIndexMatrix, numberOfElements, squareMatrixDimension);
+	ludecomposition<<<1, squareMatrixDimension, squareMatrixDimension * sizeof(int)>>>(gpuPivotMatrix, gpuMatrix, numberOfElements, squareMatrixDimension);
 
 	// Need to do forward and reverse substitution to solve for inverse matrix
 	//  using the LU Decomposition matrix, Pivot matrix and solving for ones array.
---
+++ SWroten_Project/SWrotenModule07Assignment/program.cu
@@ -0,0 +1,150 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <time.h> 
+#include <cuda.h> 
+
+#define NUMBER_OF_TESTS 5
+#define NUMBER_OF_ELEMENTS 16
+
+// Function to retrieving time
+__host__ cudaEvent_t get_time(void)
+{
+	cudaEvent_t time;
+	cudaEventCreate(&time);
+	cudaEventRecord(time);
+	return time;
+}
+
+// Kernel Function for Operating on input arrays and returns output array
+__global__ void operate(int *x, int *y, int *z)
+{
+	// Get Thread Index of Element
+	const int threadId = (blockIdx.x * blockDim.x) + threadIdx.x;
+	
+	// Exit if thread index is greater than number of elements
+	if (threadId >= NUMBER_OF_ELEMENTS) { return; }
+
+	// Perform operation on x and y to get z
+	z[threadId] = (x[threadId] * y[threadId]) + y[threadId];
+}
+
+// Main Function
+int main(int argc, char *argv[])
+{
+	// Initialize Variables
+	int devices;
+	int test = 0;
+	int *gpu_xArray;
+	int *gpu_yArray;
+	int *gpu_zArray;
+	int *cpu_xArray;
+	int *cpu_yArray;
+	int *cpu_zArray;
+	cudaEvent_t stop;
+	cudaEvent_t start;
+	cudaStream_t stream;
+	float ellapsedTimeInMs = 0.0;
+	
+	// Get Device Count
+	cudaGetDeviceCount(&devices);
+
+	// Step through each device
+	for (int indexOfDevice = 0; indexOfDevice < devices; indexOfDevice++)
+	{
+		// Initialize Local Variable
+		cudaDeviceProp prop;
+		
+		// Get Properties of this device
+		cudaGetDeviceProperties(&prop, indexOfDevice);
+
+		// Print Properties of this device
+		printf("\n");
+		printf("Device Number: %d\n", indexOfDevice);
+		printf("  Device name: %s\n", prop.name);
+		printf("  Warp Size: %i\n", prop.warpSize);
+		printf("  Max Threads Per Block: %i\n", prop.maxThreadsPerBlock);
+	}
+
+	// Create Stream
+	cudaStreamCreate(&stream);
+
+	// Allocate Memory for GPU based params
+	cudaMalloc((void **)&gpu_xArray, NUMBER_OF_ELEMENTS * sizeof(int));
+	cudaMalloc((void **)&gpu_yArray, NUMBER_OF_ELEMENTS * sizeof(int));
+	cudaMalloc((void **)&gpu_zArray, NUMBER_OF_ELEMENTS * sizeof(int));
+
+	// Allocate Memory for CPU based params
+	cudaHostAlloc((void **)&cpu_xArray, NUMBER_OF_ELEMENTS * sizeof(int), cudaHostAllocDefault);
+	cudaHostAlloc((void **)&cpu_yArray, NUMBER_OF_ELEMENTS * sizeof(int), cudaHostAllocDefault);
+	cudaHostAlloc((void **)&cpu_zArray, NUMBER_OF_ELEMENTS * sizeof(int), cudaHostAllocDefault);
+	
+	// Run Test Multiple times for better understanding
+	while (test < NUMBER_OF_TESTS)
+	{
+		printf("\n");
+		printf("Test %i: \n", test);
+
+		// Assign random Numbers for xArray and yArray
+		for (int i = 0; i < NUMBER_OF_ELEMENTS; i++)
+		{
+			cpu_xArray[i] = (rand() % 100);
+			cpu_yArray[i] = (rand() % 100);
+			cpu_zArray[i] = 0;
+		}
+
+		// Async memcopy from host to device using stream
+		cudaMemcpyAsync(gpu_xArray, cpu_xArray, NUMBER_OF_ELEMENTS * sizeof(int), cudaMemcpyHostToDevice, stream);
+		cudaMemcpyAsync(gpu_yArray, cpu_yArray, NUMBER_OF_ELEMENTS * sizeof(int), cudaMemcpyHostToDevice, stream);
+		cudaMemcpyAsync(gpu_zArray, cpu_zArray, NUMBER_OF_ELEMENTS * sizeof(int), cudaMemcpyHostToDevice, stream);
+
+		// Keep Track of Start Time
+		start = get_time();
+
+		//Operate on Input Arrays using Stream
+		operate<<<1, NUMBER_OF_ELEMENTS, 0, stream>>>(gpu_xArray, gpu_yArray, gpu_zArray);
+		
+		// Wait for all Streams to complete
+		cudaStreamSynchronize(stream);
+
+		// Keep Track of Stop Time 
+		stop = get_time();
+
+		// Synchronize Events
+		ellapsedTimeInMs = 0;
+		cudaEventSynchronize(stop);
+		cudaEventElapsedTime(&ellapsedTimeInMs, start, stop);
+
+		// Copy result of operation from GPU to CPU Memory
+		cudaMemcpy(cpu_zArray, gpu_zArray, NUMBER_OF_ELEMENTS * sizeof(int), cudaMemcpyDeviceToHost);
+
+		// Iterate through the arrays and print 
+		for (int i = 0; i < NUMBER_OF_ELEMENTS; i++)
+		{
+			printf("  ((%i * %i) + %i) = %i -> %d\n", cpu_xArray[i], cpu_yArray[i], cpu_yArray[i], cpu_zArray[i], (((cpu_xArray[i] * cpu_yArray[i]) + cpu_yArray[i]) == cpu_zArray[i]));
+		}
+
+		// Write out total time to complete
+		printf("Results:\n");
+		printf("  Time: %f ms\n", ellapsedTimeInMs);
+
+		// iterate test
+		test++;
+	}
+
+	// Free Up Memory
+	cudaFreeHost(cpu_xArray);
+	cudaFreeHost(cpu_yArray);
+	cudaFreeHost(cpu_zArray);
+	cudaFree(gpu_xArray);
+	cudaFree(gpu_yArray);
+	cudaFree(gpu_zArray);
+
+	// Reset Device
+	cudaDeviceReset();
+	
+	// return
+	return EXIT_SUCCESS;
+}
